58be2173ad9a5992d2e4624f74ac6631a60ac5b1
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-03-14 12:04:29 -0700

    Iterating Over an Array With for

---
+++ exercise2.js
@@ -0,0 +1,13 @@
+calculateRectangleArea(length, width) {
+    return length * width;
+}
+calculateTriangleArea(base, height) {
+    return base * height / 2;
+}
+calculateCircleArea(radius) {
+    return Math.PI * radius * radius;
+}
+
+// rectangle: length × width
+// triangle: base × height / 2
+// circle: π × radius2
